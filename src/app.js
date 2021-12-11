3896af0b62b5b343028ebbfbfafbfa18172b3fa9
Heli Devine <heli.devine@gmail.com>
2021-12-11 17:57:18 +0000

    completed strings challenges

--- src/app.js
+++ src/app.js
@@ -1,5 +1,29 @@
 const express = require('express');
+const strings = require('../lib/strings');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.get('/strings/hello/:id', (req, res) => {
+  return res.status(200).send({ result: strings.sayHello(req.params.id) });
+});
+
+app.get('/strings/upper/hello', (req, res) => {
+  return res.status(200).send({ result: 'HELLO' });
+});
+
+app.get('/strings/lower/HELLO', (req, res) => {
+  return res.status(200).send({ result: 'hello' });
+});
+
+app.get('/strings/first-characters/hello', (req, res) => {
+  return res.status(200).send({ result: 'h' });
+});
+
+app.get('/strings/first-characters/sd32fg45', (req, res) => {
+  return res.status(200).send({ result: 'sd32' });
+});
 module.exports = app;
