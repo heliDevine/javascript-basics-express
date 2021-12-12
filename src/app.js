cf3d8afa2136b3cff65cbc0d6546cb293f4d3655
Heli Devine <heli.devine@gmail.com>
2021-12-12 13:02:09 +0000

    working with numbers

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const strings = require('../lib/strings');
+const numbers = require('../lib/numbers');
 
 const app = express();
 
@@ -26,4 +27,52 @@ app.get('/strings/first-characters/hello', (req, res) => {
 app.get('/strings/first-characters/sd32fg45', (req, res) => {
   return res.status(200).send({ result: 'sd32' });
 });
+
+// app.get('/numbers/add/2/and/1', (req, res) => {
+//   return res.status(200).send({ result: 3 });
+// });
+
+// app.get('/numbers/add/12/and/0', (req, res) => {
+//   return res.status(200).send({ result: 12 });
+// });
+
+// app.get('/numbers/add/10/and/-5', (req, res) => {
+//   return res.status(200).send({ result: 5 });
+// });
+
+/// THIS works///
+// app.get('/numbers/add/:id/and/:id2', (req, res) => {
+//   return res.status(200).send({ result: 3 });
+// });
+/// THIS works///
+// app.get('/numbers/add/:id/and/:id2', (req, res) => {
+//   const a = parseInt(req.params.id);
+//   const b = parseInt(req.params.id2);
+//   return res.status(200).send({ result: numbers.add(a, b) });
+// });
+
+app.get('/numbers/add/:id/and/:id2', (req, res) => {
+  const a = parseInt(req.params.id);
+  const b = parseInt(req.params.id2);
+  return Number.isNaN(a) || Number.isNaN(b)
+    ? res.status(400).send({ error: 'Parameters must be valid numbers.' })
+    : res.status(200).send({ result: numbers.add(a, b) });
+});
+
+/// // WHY THIS DOESN' WORK//////
+// app.get('/numbers/add/:id/and/:id2', (req, res) => {
+//   const a = parseInt(req.params.id);
+//   const b = parseInt(req.params.id2); {
+
+//    if (Number.isNan(a) || Number.isNan(b))
+//    return res.status(400).send({ error: 'Parameters must be valid numbers.' })
+//   }
+//   else
+
+//   {
+
+//     return res.status(200).send({ result: numbers.add(a, b) });
+//   };
+// });
+
 module.exports = app;
