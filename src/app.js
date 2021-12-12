f71b907d8dafa81a297c661096e03b6deb965674
Heli Devine <heli.devine@gmail.com>
2021-12-12 17:57:30 +0000

    still working with numbers

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const strings = require('../lib/strings');
 const numbers = require('../lib/numbers');
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
@@ -75,7 +76,63 @@ app.get('/numbers/add/:id/and/:id2', (req, res) => {
 //   };
 // });
 
+/// SUBSTRACT////
 
+app.get('/numbers/subtract/:id/from/:id2', (req, res) => {
+  const a = parseInt(req.params.id);
+  const b = parseInt(req.params.id2);
 
+  return Number.isNaN(a) || Number.isNaN(b)
+    ? res.status(400).send({ error: 'Parameters must be valid numbers.' })
+    : res.status(200).send({ result: numbers.subtract(b, a) });
+});
+/// MULTPLY////
+
+// app.post('/numbers/multiply', (req, res) => {
+//   return res.status(200).json({ result: numbers.multiply(req.body.a, req.body.b) });
+// });
+
+// app.post('/numbers/multiply', (req, res) => {
+//   return Number.isNaN(req.body.a) || Number.isNaN(req.bodyb)
+//     ? res.status(400).send({ error: 'Parameters "a" and "b" are required.' })
+//     : res.status(200).json({ result: numbers.multiply(req.body.a, req.body.b) });
+// });
+
+/// / fails first two tests, passes third////
+app.post('/numbers/multiply', (req, res) => {
+  const a = parseInt(req.body);
+  const b = parseInt(req.body);
+  return Number.isNaN(a) || Number.isNaN(b)
+    ? res.status(400).send({ error: 'Parameters "a" and "b" must be valid numbers.' })
+    : res.status(200).json({ result: numbers.multiply(req.body.a, req.body.b) });
+});
+
+/// / passes first two tests, fails third////
+// app.post('/numbers/multiply', (req, res) => {
+//   return Number.isNaN(req.body.a) || Number.isNaN(req.bodyb)
+//     ? res.status(400).send({ error: 'Parameters "a" and "b" must be valid numbers.' })
+//     : res.status(200).json({ result: numbers.multiply(req.body.a, req.body.b) });
+// });
+
+/// / passes first two tests, fails third////
+
+// app.post('/numbers/multiply', (req, res) => {
+//   const a = req.body;
+//   const b = req.body;
+//   return Number.isNaN(a) || Number.isNaN(b)
+//     ? res.status(400).send({ error: 'Parameters "a" and "b" must be valid numbers.' })
+//     : res.status(200).json({ result: numbers.multiply(req.body.a, req.body.b) });
+// });
+
+/// ///////DIVIDE/////////
+
+// app.post('/numbers/divide', (req, res) => {
+//   return Number.isNaN(req.body.a) || Number.isNaN(req.bodyb)
+//     ? res.status(400).send({ error: 'Parameters "a" and "b" must be valid numbers.' })
+//     : res.status(200).json({ result: numbers.divide(req.body.a, req.body.b) });
+// });
+app.post('/numbers/divide', (req, res) => {
+  return res.status(200).json({ result: numbers.divide(req.body.a, req.body.b) });
+});
 
 module.exports = app;
